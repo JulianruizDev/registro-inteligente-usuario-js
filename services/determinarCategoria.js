d149f76b84aec252e40e6ca8cb7070cfee2df114
Julian Ruiz <julianruiza4@gmail.com>
2026-03-05 23:04:21 -0500

    refactor: mejorar funcion determinarCategoria

--- services/determinarCategoria.js
+++ services/determinarCategoria.js
@@ -1,8 +1,16 @@
 function determinarCategoria(edad) {
 
-  if (edad <= 12) return "Niño";
-  if (edad <= 17) return "Adolescente";
-  if (edad <= 64) return "Adulto";
+  if (edad <= 12) {
+    return "Niño";
+  }
+
+  if (edad <= 17) {
+    return "Adolescente";
+  }
+
+  if (edad <= 64) {
+    return "Adulto";
+  }
 
   return "Tercera edad";
 
